8c34ac5188d19a6bdd6bb4aeeadc4350b3bcc128
Alexander Benavente <aibenaventea@gmail.com>
2022-11-12 23:14:02 -0300

    commit inicial

---
+++ app.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer((request, response) => {
+    if(request.url === '/') {
+        fs.readFile('index.html', 'utf8', (errors, contents) => {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if (request.url === "/ninjas.html") {
+        fs.readFile('ninjas.html', 'utf8', (errors, contents) => {
+            response.writeHead(200, {'Content-type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if (request.url === "/dojos/dojos.html") {
+        fs.readFile('./dojos/dojos.html', 'utf8', (errors, contents) => {
+            response.writeHead(200, {'Content-type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }       
+    else {
+        response.end('404 Not Found');
+    }
+});
+let puerto = 6789
+server.listen(puerto);
+console.log(`Running in localhost at port ${puerto}`);
